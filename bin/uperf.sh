0dfb60d840e1308666e230afbb21bccf4ffe15c8
Bela Ban <belaban@mailbox.org>
2021-07-16 16:24:43 +0200

    ns

--- bin/uperf.sh
+++ bin/uperf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -classpath ./target/classes:target/dependency/* org.jgroups.tests.perf.UPerf -props shm.xml "$@";
+java -Xmx1G  -Dagrona.disable.bounds.checks=true -classpath ./target/classes:target/dependency/* org.jgroups.tests.perf.UPerf -props shm.xml "$@";
